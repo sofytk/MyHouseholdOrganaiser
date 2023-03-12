6769e26d577a7052a61e428fb7d78cad7ee0205b
sofytk <sofata31@gmail.com>
2023-03-12 17:49:17 +0300

    newTaskActivity

---
+++ app/src/main/java/com/example/myhouseholdorganaiser/WhoDoingAdapter.java
@@ -0,0 +1,4 @@
+package com.example.myhouseholdorganaiser;
+
+public class WhoDoingAdapter {
+}
--- app/src/main/java/com/example/myhouseholdorganaiser/activities/NewTaskActivity.java
+++ app/src/main/java/com/example/myhouseholdorganaiser/activities/NewTaskActivity.java
@@ -6,26 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.example.myhouseholdorganaiser.R;
 import com.example.myhouseholdorganaiser.databinding.ActivityNewTaskBinding;
 import com.example.myhouseholdorganaiser.task.MyTask;
-import com.noowenz.customdatetimepicker.CustomDateTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class NewTaskActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+public class NewTaskActivity extends AppCompatActivity{
     ActivityNewTaskBinding binding;
     public static final List<MyTask> tasksList = new ArrayList<>();
     private MyTask task;
+    Calendar calendar = Calendar.getInstance();
 
 
     @SuppressLint("MissingInflatedId")
@@ -36,33 +41,48 @@ public class NewTaskActivity extends AppCompatActivity implements DatePickerDial
 
         binding = ActivityNewTaskBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                binding.currentTime.setText(new SimpleDateFormat("H:mm").format(calendar.getTime()) );
+
+            }
+        };
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                binding.currentDate.setText(new SimpleDateFormat("d EEEE").format(calendar.getTime()));
+            }
+        };
+
+        binding.currentDate.setOnClickListener(view->{
+            new DatePickerDialog(NewTaskActivity.this, R.style.TimeDatePickerTheme, dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH))
+                    .show();
+        });
+
+        binding.currentTime.setOnClickListener(view->{
+           TimePickerDialog timePickerDialog = new TimePickerDialog(NewTaskActivity.this, R.style.TimeDatePickerTheme,
+                    timeSetListener,
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE), true);
+           timePickerDialog.show();
+
+        });
+
         binding.save.setOnClickListener(view->{
-            task = new MyTask(binding.newTaskName.getText().toString(), binding.dateOfTask.getText().toString());
+            task = new MyTask(binding.newTaskName.getText().toString(), ("Выполнить до " + binding.currentTime.getText()).toString());
             tasksList.add(task);
             Intent intent = new Intent(NewTaskActivity.this, MainActivity.class);
             intent.putExtra("newTask", task);
             startActivity(intent);
         });
-        binding.currentDate.setOnClickListener(view->{
-            com.example.myhouseholdorganaiser.calendar.DatePicker mDatePickerDialogFragment;
-            mDatePickerDialogFragment = new com.example.myhouseholdorganaiser.calendar.DatePicker();
-            mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
-        });
+
     }
-
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar mCalendar = Calendar.getInstance();
-        mCalendar.set(Calendar.YEAR, year);
-        mCalendar.set(Calendar.MONTH, month);
-        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
-        binding.dateOfTask.setText(selectedDate);
-    }
-
-    public String getDate() {
-        return binding.dateOfTask.toString();
-    }
-
 }
